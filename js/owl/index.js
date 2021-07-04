d9674d375983c03a3647ccaa1277c6382c07701e
cezar-gh <cezar.vargas.fernandes@gmail.com>
2021-07-03 21:58:52 -0300

    Alterada a div da class=filme-principal - class=container na qual foi dada a nova cor linear-gradient(rgb(39, 2, 28),rgb(15, 1, 11))100%  para se aproximar à cor da imagem oscar.jpg

---
+++ js/owl/index.js
@@ -0,0 +1,15 @@
+function ativaScrollSuave(selector) {
+
+    $(selector).click(function(event){
+        event.preventDefault();
+
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);  
+    });
+}
+
+ativaScrollSuave('a[href*=galeria-famosos]');   
+ativaScrollSuave('a[href*=topo]');
